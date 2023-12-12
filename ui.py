97c73e193cd428d0eee4502ad5e720bad5094341
Rosti415 <116059105+Rosti415@users.noreply.github.com>
2023-12-12 18:55:47 +0200

    Delete ui.py

--- ui.py
+++
@@ -1,9 +0,0 @@
-from ursina import *
-
-class Menu(Entity):
-    def __init__(self, actions, **kwargs):#змінна кількіть агрументів(kwagrs)
-        super().__init__(parent = camera.ui,ignore_paused = True, **kwargs)
-
-        self.main_menu = Entity(parent =self, enabled = True)
-        self.bg = Sprite(parent =self.main_menu,scale = 0.1, texture = "assets/bag.jpg", color = color.dark_gray, z = 1)
-
